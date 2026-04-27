6b930929083f4dfe9884a5f6819c12dae6ba6466
cultivate27 <raziqdanish777@gmail.com>
2026-04-27 09:41:49 +0700

    Initial commit

---
+++ NeoFSM/FsmEngine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace NeoFSM
+{
+    public enum State { S, A, B, C }
+
+    public class FsmEngine
+    {
+        public State CurrentState { get; private set; }
+        public List<State> History { get; private set; }
+
+        public FsmEngine() => Reset();
+
+        public void Reset()
+        {
+            CurrentState = State.S;
+            History = new List<State> { State.S };
+        }
+
+        public bool Validate(string input)
+        {
+            Reset();
+            foreach (char c in input)
+            {
+                if (c == '0') Transition0();
+                else if (c == '1') Transition1();
+                else { CurrentState = State.C; } // Invalid char
+
+                History.Add(CurrentState);
+            }
+            return CurrentState == State.B;
+        }
+
+        private void Transition0()
+        {
+            CurrentState = CurrentState switch
+            {
+                State.S => State.A,
+                State.A => State.C,
+                State.B => State.A,
+                _ => State.C
+            };
+        }
+
+        private void Transition1()
+        {
+            CurrentState = CurrentState switch
+            {
+                State.S => State.B,
+                State.A => State.B,
+                State.B => State.B,
+                _ => State.C
+            };
+        }
+    }
+}
---
+++ NeoFSM/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Media;
+
+namespace NeoFSM
+{
+    public partial class MainWindow : Window
+    {
+        private readonly FsmEngine _engine = new FsmEngine();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bool isWindowsDark = IsWindowsDarkMode();
+
+            ThemeToggle.IsChecked = isWindowsDark;
+
+            ApplyTheme(isWindowsDark);
+
+            AppVersionRun.Text = "v1.0.0";
+        }
+
+        private void InputTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                ResultPanel.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            ResultPanel.Visibility = Visibility.Visible;
+
+            bool isAccepted = _engine.Validate(input);
+
+            UpdateResultUI(isAccepted, string.Join(" → ", _engine.History));
+        }
+
+        private void UpdateResultUI(bool isAccepted, string trace)
+        {
+            StatusResult.Text = isAccepted ? "ACCEPTED" : "REJECTED";
+            StatusResult.Foreground = isAccepted ? Brushes.Green : Brushes.Red;
+
+            StatusBorder.Background = isAccepted
+                ? new SolidColorBrush(Color.FromArgb(35, 0, 255, 0))
+                : new SolidColorBrush(Color.FromArgb(35, 255, 0, 0));
+
+            RouteDisplay.Text = trace;
+        }
+
+        private bool IsWindowsDarkMode()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"))
+                {
+                    // 0 = Dark Mode, 1 = Light Mode
+                    return (int)(key?.GetValue("AppsUseLightTheme") ?? 1) == 0;
+                }
+            }
+            catch { return false; }
+        }
+
+        private void ThemeToggle_Click(object sender, RoutedEventArgs e)
+        {
+            ApplyTheme(ThemeToggle.IsChecked ?? false);
+        }
+
+        private void ApplyTheme(bool isDark)
+        {
+            var darkBg = new SolidColorBrush(Color.FromRgb(30, 30, 30));
+            var controlBg = new SolidColorBrush(Color.FromRgb(45, 45, 45));
+            var borderDark = new SolidColorBrush(Color.FromRgb(65, 65, 65));
+
+            if (isDark)
+            {
+                this.Background = darkBg;
+                this.Foreground = Brushes.White;
+                InputTextBox.Background = controlBg;
+                InputTextBox.Foreground = Brushes.White;
+                InputTextBox.BorderBrush = borderDark;
+
+                StatusTitle.Foreground = Brushes.LightGray;
+                RouteDisplay.Foreground = Brushes.LightGray;
+            }
+            else
+            {
+                this.Background = new SolidColorBrush(Color.FromRgb(243, 243, 243));
+                this.Foreground = Brushes.Black;
+                InputTextBox.Background = Brushes.White;
+                InputTextBox.Foreground = Brushes.Black;
+                InputTextBox.BorderBrush = Brushes.LightGray;
+
+                StatusTitle.Foreground = Brushes.Gray;
+                RouteDisplay.Foreground = Brushes.DimGray;
+            }
+        }
+    }
+}
